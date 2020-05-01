5dff4c2dafee704fb1ab521f834d7e68bce6c36b
Kovbasiuk <roberthnc98@gmail.com>
2020-05-01 01:00:43 +0100

    Initial Commit

---
+++ orderaturban2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  orderaturban2
+//
+//  Created by Robert Kovbasiuk on 01/05/2020.
+//  Copyright © 2020 RK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
